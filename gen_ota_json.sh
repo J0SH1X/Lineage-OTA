fa0227404cac4ea415901cbfc5a9a050b0a65d19
J0SH1X <aljoshua.hell@gmail.com>
2020-03-29 20:37:57 +0200

    some fixes suggested by shellcheck

--- gen_ota_json.sh
+++ gen_ota_json.sh
@@ -1,12 +1,13 @@
-d=`date +%Y%m%d`
-oldd=`cat judyln.json | grep filename | cut -d '-' -f 3`
-md5=`md5sum ../out/target/product/judyln/lineage-17.1-${d}-UNOFFICIAL-judyln.zip | cut -d ' ' -f 1`
-oldmd5=`cat judyln.json | grep '"id"' | cut -d':' -f 2`
-utc=`cat ../out/target/product/judyln/system/build.prop | grep ro.build.date.utc | cut -d '=' -f 2`
-oldutc=`cat judyln.json | grep datetime | cut -d ':' -f 2`
-size=`wc -c ../out/target/product/judyln/lineage-17.1-${d}-UNOFFICIAL-judyln.zip | cut -d ' ' -f 1`
-oldsize=`cat judyln.json | grep size | cut -d ':' -f 2`
-oldurl=`cat judyln.json | grep url | cut -d ' ' -f 8`
+#!/bin/bash
+d=$(date +%Y%m%d)
+oldd=$(cat judyln.json | grep filename | cut -d '-' -f 3)
+md5=$(md5sum ../out/target/product/judyln/lineage-17.1-"${d}"-UNOFFICIAL-judyln.zip | cut -d ' ' -f 1)
+oldmd5=$(cat judyln.json | grep '"id"' | cut -d':' -f 2)
+utc=$(cat ../out/target/product/judyln/system/build.prop | grep ro.build.date.utc | cut -d '=' -f 2)
+oldutc=$(cat judyln.json | grep datetime | cut -d ':' -f 2)
+size=$(wc -c ../out/target/product/judyln/lineage-17.1-"${d}"-UNOFFICIAL-judyln.zip | cut -d ' ' -f 1)
+oldsize=$(cat judyln.json | grep size | cut -d ':' -f 2)
+oldurl=$(cat judyln.json | grep url | cut -d ' ' -f 8)
 
 #This is where the magic happens
 
@@ -16,5 +17,5 @@ sed -i "s!${oldutc}! \"${utc}\",!g" judyln.json
 sed -i "s!${oldsize}! \"${size}\",!g" judyln.json
 sed -i "s!${oldd}!${d}!" judyln.json
 echo Enter the new Download URL
-read url
+read -r url
 sed -i "s!${oldurl}! \"${url}\",!g" judyln.json
