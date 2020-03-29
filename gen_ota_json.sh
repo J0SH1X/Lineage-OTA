db9791a97cc029de4f6383a4bfe5270ca923dfac
J0SH1X <aljoshua.hell@gmail.com>
2020-03-29 20:07:45 +0200

    finish script functions

--- gen_ota_json.sh
+++ gen_ota_json.sh
@@ -6,7 +6,7 @@ utc=`cat ../out/target/product/judyln/system/build.prop | grep ro.build.date.utc
 oldutc=`cat judyln.json | grep datetime | cut -d ':' -f 2`
 size=`wc -c ../out/target/product/judyln/lineage-17.1-${d}-UNOFFICIAL-judyln.zip | cut -d ' ' -f 1`
 oldsize=`cat judyln.json | grep size | cut -d ':' -f 2`
-
+oldurl=`cat judyln.json | grep url | cut -d ' ' -f 8`
 
 #Debugging im not good with shell scripts
 echo DATE
@@ -30,4 +30,6 @@ sed -i "s!${oldmd5}! \"${md5}\",!g" judyln.json
 sed -i "s!${oldutc}! \"${utc}\",!g" judyln.json
 sed -i "s!${oldsize}! \"${size}\",!g" judyln.json
 sed -i "s!${oldd}!${d}!" judyln.json
-
+echo Enter the new Download URL
+read url
+sed -i "s!${oldurl}! \"${url}\",!g" judyln.json
