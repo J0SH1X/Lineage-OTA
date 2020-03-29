c5794fda38eafc76d1ea2d49f7957ab3da79a596
J0SH1X <aljoshua.hell@gmail.com>
2020-03-29 19:58:46 +0200

    update script again were nearly done

--- gen_ota_json.sh
+++ gen_ota_json.sh
@@ -4,17 +4,30 @@ md5=`md5sum ../out/target/product/judyln/lineage-17.1-${d}-UNOFFICIAL-judyln.zip
 oldmd5=`cat judyln.json | grep '"id"' | cut -d':' -f 2`
 utc=`cat ../out/target/product/judyln/system/build.prop | grep ro.build.date.utc | cut -d '=' -f 2`
 oldutc=`cat judyln.json | grep datetime | cut -d ':' -f 2`
+size=`wc -c ../out/target/product/judyln/lineage-17.1-${d}-UNOFFICIAL-judyln.zip | cut -d ' ' -f 1`
+oldsize=`cat judyln.json | grep size | cut -d ':' -f 2`
+
 
 #Debugging im not good with shell scripts
+echo DATE
 echo ${d}
 echo ${oldd}
+echo MD5
 echo ${md5}
 echo ${oldmd5}
+echo UTC
 echo ${utc}
 echo ${oldutc}
+echo SIZE
+echo ${size}
+echo ${oldsize}
+
+
 #This is where the magic happens
 
 
-sed -i "s!${oldmd5}!"${md5}",!" judyln.json
-sed -i "s!${oldutc}!"${utc}",!" judyln.json
-sed -i "s!${oldd}!"${d}",!" judyln.json
+sed -i "s!${oldmd5}! \"${md5}\",!g" judyln.json
+sed -i "s!${oldutc}! \"${utc}\",!g" judyln.json
+sed -i "s!${oldsize}! \"${size}\",!g" judyln.json
+sed -i "s!${oldd}!${d}!" judyln.json
+
