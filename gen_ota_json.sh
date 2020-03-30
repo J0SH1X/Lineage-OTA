24449e75f23713326537e358aeb800210c2975c3
J0SH1X <aljoshua.hell@gmail.com>
2020-03-30 10:49:56 +0200

    finalize script with shellcheck

--- gen_ota_json.sh
+++ gen_ota_json.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 d=$(date +%Y%m%d)
-oldd=$(cat judyln.json | grep filename | cut -d '-' -f 3)
+oldd=$(grep filename judyln.json | cut -d '-' -f 3)
 md5=$(md5sum ../out/target/product/judyln/lineage-17.1-"${d}"-UNOFFICIAL-judyln.zip | cut -d ' ' -f 1)
-oldmd5=$(cat judyln.json | grep '"id"' | cut -d':' -f 2)
-utc=$(cat ../out/target/product/judyln/system/build.prop | grep ro.build.date.utc | cut -d '=' -f 2)
-oldutc=$(cat judyln.json | grep datetime | cut -d ':' -f 2)
+oldmd5=$(grep '"id"' judyln.json | cut -d':' -f 2)
+utc=$(grep ro.build.date.utc ../out/target/product/judyln/system/build.prop | cut -d '=' -f 2)
+oldutc=$(grep datetime judyln.json | cut -d ':' -f 2)
 size=$(wc -c ../out/target/product/judyln/lineage-17.1-"${d}"-UNOFFICIAL-judyln.zip | cut -d ' ' -f 1)
-oldsize=$(cat judyln.json | grep size | cut -d ':' -f 2)
-oldurl=$(cat judyln.json | grep url | cut -d ' ' -f 9)
+oldsize=$(grep size judyln.json | cut -d ':' -f 2)
+oldurl=$(grep url judyln.json | cut -d ' ' -f 9)
 
 #This is where the magic happens
 
