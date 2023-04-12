7afb95304af617fc0ac4cfd52800bd1f15fb0b45
j0sh1x <aljoshua.hell@gmail.com>
2023-04-12 23:07:55 +0200

    R8Q: inital OTA support

--- gen_ota_json.sh
+++ gen_ota_json.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 DEVICE=$1
-
+REPOS="${@:2}"
 
 d=$(date +%Y%m%d)
 
-VERSION=$(git rev-parse --abbrev-ref HEAD)
-FILENAME="${VERSION}"-"${d}"-UNOFFICIAL-"${DEVICE}".zip
-
+FILENAME=lineage-20.0-"${d}"-UNOFFICIAL-"${DEVICE}".zip
 
 oldd=$(grep filename $DEVICE.json | cut -d '-' -f 3)
 md5=$(md5sum ../out/target/product/$DEVICE/$FILENAME | cut -d ' ' -f 1)
@@ -16,18 +14,7 @@ utc=$(grep ro.build.date.utc ../out/target/product/$DEVICE/system/build.prop | c
 oldutc=$(grep datetime $DEVICE.json | cut -d ':' -f 2)
 size=$(wc -c ../out/target/product/$DEVICE/$FILENAME | cut -d ' ' -f 1)
 oldsize=$(grep size $DEVICE.json | cut -d ':' -f 2)
-oldurl=$(grep url $DEVICE.json | cut -d ':' -f 3)
-oldurl="\"https:${oldurl}"
-devicerepopath=$(find ../device | grep -i ${DEVICE} | head  -1 | cut -d '.' -f 3)
-manufacturer=$(echo $devicerepopath | cut -d '/' -f 3)
-devicecommonrepo=$(cat ../$devicerepopath/BoardConfig.mk | grep -i 'common/Bo' | cut -d '/' -f 3)
-deviceplatform=$(echo $devicecommonrepo | cut -d '-' -f1)
-changelogdevicepath=device/$manufacturer/$DEVICE
-changelogdevicecommonpath=device/$manufacturer/$devicecommonrepo
-changelogkernelpath=kernel/$manufacturer/$deviceplatform
-changelogvendorpath=vendor/$manufacturer
-REPOS=(${changelogdevicepath} ${changelogdevicecommonpath} ${changelogkernelpath} ${changelogvendorpath})
-GITORIGIN=$(git config --get remote.origin.url | cut -d '/' -f4)
+oldurl=$(grep url $DEVICE.json | cut -d ' ' -f 9)
 
 # Generate the Changelog
 # Clear the changelog file
@@ -49,13 +36,7 @@ sed -i "s!${oldsize}! \"${size}\",!g" $DEVICE.json
 sed -i "s!${oldd}!${d}!" $DEVICE.json
 #echo Generate Download URL
 TAG=$(echo "${DEVICE}-${d}")
-url="https://github.com/${GITORIGIN}/Lineage-OTA/releases/download/${TAG}/${FILENAME}"
+url="https://github.com/J0SH1X/Lineage-OTA/releases/download/${TAG}/${FILENAME}"
 sed -i "s!${oldurl}!\"${url}\",!g" $DEVICE.json
 
-git add $DEVICE.json
-git add changelog.txt
-git commit -m "Update ${DEVICE} to ${d}"
-git push https://github.com/${GITORIGIN}/Lineage-OTA ${VERSION}
-
 hub release create -a ../out/target/product/$DEVICE/$FILENAME -a changelog.txt -m "${TAG}" "${TAG}"
-
